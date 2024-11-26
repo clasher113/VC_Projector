d37c5567f8e0acb71bf545e28589fa176cba3494
@clasher113 <kizinvanya@gmail.com>
2024-11-26 18:47:00 +0200

    search VC process implementation add status bar

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,49 @@
 #include <iostream>
 
 #include <SFML/Graphics.hpp>
+#include <filesystem>
 
 #ifdef _WIN32
 #pragma comment(lib, "winmm.lib")
 #pragma comment(lib, "Dwmapi.lib")
 #include <dwmapi.h>
+#include <tlhelp32.h>
+#include <psapi.h>
 #endif // _WIN32
+
+namespace fs = std::filesystem;
+
+enum class Status {
+	WAITING = 1,
+	CONNECTED,
+	READY,
+	CAPTURING
+};
 
 unsigned int framerate = 30;
 float updateTimer = 0.f;
 float updateInterval = 1.f / framerate;
-const uint16_t screenWidth = 180;
+const uint16_t screenWidth = 240;
 const uint16_t screenHeigth = 120;
 //const uint8_t chunksNumY = (screenHeigth / CHUNK_D) + 1;
 sf::Vector2u displayReadPos(240, 240);
 sf::Vector2u displayReadSize(screenWidth, screenHeigth);
+const sf::Vector2u statusContainerSize(300, 30);
 
 constexpr float borderThickness = 3.f;
+HANDLE hProcess = nullptr;
+fs::path processDirectory;
+
+static void setWindowSize(sf::RenderWindow& window, const sf::Vector2u& size);
+static void updateBorder(sf::RectangleShape& border);
+static void updateStatusSprite(sf::Sprite& sprite);
+static void setStatus(sf::Text& statusText, Status& currentStatus, Status newStatus);
 
 int main(){
 
 	sf::RenderWindow window(sf::VideoMode(320, 240), "Projector server", sf::Style::None);
 	window.setFramerateLimit(framerate);
+	setWindowSize(window, displayReadSize);
 
 	MARGINS margins{};
 	margins.cxLeftWidth = -1;
@@ -45,14 +66,28 @@ int main(){
 	bmi.bmiHeader.biWidth = displayReadSize.x;
 	bmi.bmiHeader.biSize = sizeof(BITMAPINFO);
 
-	sf::Color* pixels = new sf::Color[displayReadSize.x * displayReadSize.y];
+	sf::Color* rgbPixels = new sf::Color[displayReadSize.x * displayReadSize.y];
+	uint8_t* monochromePixels = new uint8_t[displayReadSize.x * displayReadSize.y];
+	Status currentStatus = Status::WAITING;
 
 	sf::RectangleShape border;
 	border.setFillColor(sf::Color::Transparent);
 	border.setOutlineColor(sf::Color::Yellow);
 	border.setOutlineThickness(borderThickness);
 	border.setPosition(sf::Vector2f(borderThickness, borderThickness));
-	border.setSize(sf::Vector2f(window.getSize().x - borderThickness * 2, window.getSize().y - borderThickness * 2));
+	updateBorder(border);
+
+	sf::Font font;
+	font.loadFromFile("C://Windows//Fonts//arial.ttf");
+
+	sf::Text statusText("", font, 20U);
+	setStatus(statusText, currentStatus, currentStatus);
+
+	sf::RenderTexture statusContainer;
+	statusContainer.create(statusContainerSize.x, statusContainerSize.y);
+
+	sf::Sprite statusContainerSprite(statusContainer.getTexture());
+	updateStatusSprite(statusContainerSprite);
 
 	sf::Vector2i grabbedOffset;
 	bool grabbedWindow = false;
@@ -62,6 +97,54 @@ int main(){
 	while (window.isOpen()) {
 		const float delta = clock.restart().asSeconds();
 
+		if (currentStatus == Status::WAITING){
+			static float searchProcessTimer = 0.f;
+			searchProcessTimer += delta;
+			if (searchProcessTimer > 1) {
+				searchProcessTimer = 0;
+
+				PROCESSENTRY32 entry;
+				entry.dwSize = sizeof(PROCESSENTRY32);
+
+				HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+				if (Process32First(snapshot, &entry) == TRUE) {
+					while (Process32Next(snapshot, &entry) == TRUE) {
+						if (stricmp(entry.szExeFile, "VoxelEngine-Cpp.exe") == 0) {
+							hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, entry.th32ProcessID);
+							WCHAR path[MAX_PATH]{};
+							GetModuleFileNameExW(hProcess, NULL, path, MAX_PATH);
+							processDirectory = fs::path(path).parent_path();
+							setStatus(statusText, currentStatus, Status::CONNECTED);
+						}
+					}
+				}
+
+				CloseHandle(snapshot);
+			}
+		}
+		else if (currentStatus == Status::CONNECTED) {
+		}
+		else if (currentStatus == Status::READY){
+		}
+		else if (currentStatus == Status::CAPTURING){
+			BitBlt(hCaptureDC, 0, 0, displayReadSize.x, displayReadSize.y, desktopHdc, window.getPosition().x, window.getPosition().y, SRCCOPY);
+			GetDIBits(hCaptureDC, hCaptureBitmap, 0, displayReadSize.y, &rgbPixels[0], &bmi, DIB_RGB_COLORS);
+
+			for (size_t i = 0; i < displayReadSize.x * displayReadSize.y; i++) {
+				const sf::Color& rgbPixel = rgbPixels[i];
+				monochromePixels[i] = 0.2126 * (rgbPixel.r / 255.f) + 0.7152 * (rgbPixel.g / 255.f) + 0.0722 * (rgbPixel.b / 255.f);
+			}
+		}
+
+		if (currentStatus != Status::WAITING){
+			DWORD ret = 0;
+			if (hProcess) GetExitCodeProcess(hProcess, &ret);
+			if (ret != STILL_ACTIVE) {
+				CloseHandle(hProcess);
+				setStatus(statusText, currentStatus, Status::WAITING);
+			}
+		}
 
 		sf::Event e;
 		while(window.pollEvent(e)){
@@ -80,24 +163,47 @@ int main(){
 				if (grabbedWindow)
 					window.setPosition(sf::Mouse::getPosition() + grabbedOffset);
 			}
-			else if (e.type == sf::Event::KeyPressed) {
-				if (e.key.code == sf::Keyboard::X) {
-					//BitBlt(hCaptureDC, 0, 0, displayReadSize.x, displayReadSize.y, desktopHdc, window.getPosition().x, window.getPosition().y, SRCCOPY);
-					//GetDIBits(hCaptureDC, hCaptureBitmap, 0, displayReadSize.y, &pixels[0], &bmi, DIB_RGB_COLORS);
-
-					//sf::Image image;
-					//image.create(displayReadSize.x, displayReadSize.y, reinterpret_cast<sf::Uint8*>(pixels));
-					//image.saveToFile("test.png");
-				}
-			}
 		}
+
+		statusContainer.clear(sf::Color::Black);
+		statusContainer.draw(statusText);
+		statusContainer.display();
 
 		window.clear(sf::Color::Transparent);
 		window.draw(border);
+		window.draw(statusContainerSprite);
 		window.display();
 	}
 
-	delete[] pixels;
+	delete[] rgbPixels;
+	delete[] monochromePixels;
 
 	return 0;
+}
+
+void setWindowSize(sf::RenderWindow& window, const sf::Vector2u& size) {
+	const sf::Vector2u newSize(std::max(size.x + static_cast<unsigned int>(borderThickness * 2), statusContainerSize.x),
+							   size.y + static_cast<unsigned int>(borderThickness * 2) + statusContainerSize.y);
+	window.setSize(newSize);
+	window.setView(sf::View(sf::Vector2f(newSize.x / 2, newSize.y / 2), sf::Vector2f(newSize)));
+}
+
+void updateBorder(sf::RectangleShape& border) {
+	border.setSize(sf::Vector2f(displayReadSize.x - borderThickness * 2, displayReadSize.y - borderThickness * 2 ));
+}
+
+void updateStatusSprite(sf::Sprite& sprite) {
+	sprite.setPosition(sf::Vector2f(0.f, displayReadSize.y));
+}
+
+void setStatus(sf::Text& statusText, Status& currentStatus, Status newStatus) {
+	currentStatus = newStatus;
+	std::string statusStr;
+	switch (currentStatus) {
+		case Status::WAITING: statusStr = "Waiting for VC"; break;
+		case Status::CONNECTED: statusStr = "Connected"; break;
+		case Status::READY: statusStr = "Ready"; break;
+		case Status::CAPTURING: statusStr = "Capturing"; break;
+	}
+	statusText.setString("Status: " + statusStr);
 }
