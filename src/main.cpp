5aae540f0c629f024f57a7274b0919ac91c2d15b
@clasher113 <kizinvanya@gmail.com>
2024-11-27 18:43:42 +0200

    simple synchronization implementation bug fix

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 #include <SFML/Graphics.hpp>
 #include <filesystem>
+#include <sstream>
+#include <fstream>
 
 #ifdef _WIN32
 #pragma comment(lib, "winmm.lib")
@@ -16,28 +18,27 @@ namespace fs = std::filesystem;
 enum class Status {
 	WAITING = 1,
 	CONNECTED,
+	SYNCING,
 	READY,
 	CAPTURING
 };
 
 unsigned int framerate = 30;
-float updateTimer = 0.f;
-float updateInterval = 1.f / framerate;
-const uint16_t screenWidth = 240;
-const uint16_t screenHeigth = 120;
-//const uint8_t chunksNumY = (screenHeigth / CHUNK_D) + 1;
 sf::Vector2u displayReadPos(240, 240);
-sf::Vector2u displayReadSize(screenWidth, screenHeigth);
+sf::Vector2u displayReadSize(240, 120);
 const sf::Vector2u statusContainerSize(300, 30);
 
+constexpr float syncTimeout = 1.f;
 constexpr float borderThickness = 3.f;
 HANDLE hProcess = nullptr;
-fs::path processDirectory;
+fs::path workingDirectory;
 
 static void setWindowSize(sf::RenderWindow& window, const sf::Vector2u& size);
 static void updateBorder(sf::RectangleShape& border);
 static void updateStatusSprite(sf::Sprite& sprite);
 static void setStatus(sf::Text& statusText, Status& currentStatus, Status newStatus);
+static std::vector<std::string> readOutFile();
+static void sendMessage(const std::string& message);
 
 int main(){
 
@@ -91,11 +92,13 @@ int main(){
 
 	sf::Vector2i grabbedOffset;
 	bool grabbedWindow = false;
-
+	float syncTimer = 0.f;
 	sf::Clock clock;
 
 	while (window.isOpen()) {
 		const float delta = clock.restart().asSeconds();
+
+		auto commands = readOutFile();
 
 		if (currentStatus == Status::WAITING){
 			static float searchProcessTimer = 0.f;
@@ -114,7 +117,7 @@ int main(){
 							hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, entry.th32ProcessID);
 							WCHAR path[MAX_PATH]{};
 							GetModuleFileNameExW(hProcess, NULL, path, MAX_PATH);
-							processDirectory = fs::path(path).parent_path();
+							workingDirectory = fs::path(path).parent_path() / "export" / "VC_Projector";
 							setStatus(statusText, currentStatus, Status::CONNECTED);
 						}
 					}
@@ -123,9 +126,33 @@ int main(){
 				CloseHandle(snapshot);
 			}
 		}
-		else if (currentStatus == Status::CONNECTED) {
+		else if (currentStatus == Status::SYNCING) {
+			Status newStatus = Status::READY;
+			if (!commands.empty()) {
+				try {
+					displayReadSize = sf::Vector2u(stoi(commands.at(0)), stoi(commands.at(1)));
+					setWindowSize(window, displayReadSize);
+					updateBorder(border);
+					updateStatusSprite(statusContainerSprite);
+				}
+				catch (const std::exception& e) {
+					std::cout << "Synchonization error" << std::endl;
+					std::cout << e.what() << std::endl;
+					newStatus = Status::CONNECTED;
+				}
+				setStatus(statusText, currentStatus, newStatus);
+			}
+			else {
+				syncTimer += delta;
+				if (syncTimer > syncTimeout) {
+					syncTimer = 0;
+					std::cout << "Synchonization timeout" << std::endl;
+					setStatus(statusText, currentStatus, Status::CONNECTED);
+				}
+			}
 		}
 		else if (currentStatus == Status::READY){
+
 		}
 		else if (currentStatus == Status::CAPTURING){
 			BitBlt(hCaptureDC, 0, 0, displayReadSize.x, displayReadSize.y, desktopHdc, window.getPosition().x, window.getPosition().y, SRCCOPY);
@@ -143,6 +170,17 @@ int main(){
 			if (ret != STILL_ACTIVE) {
 				CloseHandle(hProcess);
 				setStatus(statusText, currentStatus, Status::WAITING);
+			}
+		}
+		if (currentStatus == Status::CONNECTED || currentStatus == Status::READY) {
+			if (!commands.empty()) {
+				if (commands[0] == "sync") {
+					setStatus(statusText, currentStatus, Status::SYNCING);
+					sendMessage("sync");
+				}
+				else if (commands[0] == "disconnect"){
+					setStatus(statusText, currentStatus, Status::CONNECTED);
+				}
 			}
 		}
 
@@ -189,11 +227,11 @@ void setWindowSize(sf::RenderWindow& window, const sf::Vector2u& size) {
 }
 
 void updateBorder(sf::RectangleShape& border) {
-	border.setSize(sf::Vector2f(displayReadSize.x - borderThickness * 2, displayReadSize.y - borderThickness * 2 ));
+	border.setSize(sf::Vector2f(displayReadSize.x, displayReadSize.y));
 }
 
 void updateStatusSprite(sf::Sprite& sprite) {
-	sprite.setPosition(sf::Vector2f(0.f, displayReadSize.y));
+	sprite.setPosition(sf::Vector2f(0.f, displayReadSize.y + borderThickness * 2));
 }
 
 void setStatus(sf::Text& statusText, Status& currentStatus, Status newStatus) {
@@ -202,8 +240,45 @@ void setStatus(sf::Text& statusText, Status& currentStatus, Status newStatus) {
 	switch (currentStatus) {
 		case Status::WAITING: statusStr = "Waiting for VC"; break;
 		case Status::CONNECTED: statusStr = "Connected"; break;
+		case Status::SYNCING: statusStr = "Syncing"; break;
 		case Status::READY: statusStr = "Ready"; break;
 		case Status::CAPTURING: statusStr = "Capturing"; break;
 	}
 	statusText.setString("Status: " + statusStr);
+}
+
+static std::vector<std::string> splitString(const std::string& string){
+	std::vector<std::string> result;
+	std::stringstream ss(string);
+	std::string tmp;
+	while (std::getline(ss, tmp, ':')) {
+		result.push_back(tmp);
+	}
+	return result;
+}
+
+std::vector<std::string> readOutFile() {
+	std::vector<std::string> commands;
+	fs::path outFile(workingDirectory / "out");
+	if (fs::is_regular_file(outFile)) {
+		std::ifstream file(outFile);
+		std::string line;
+		if (file.is_open()) {
+			while (getline(file, line)) {
+				auto split = splitString(line);
+				commands.insert(commands.end(), split.begin(), split.end());
+			}
+			file.close();
+		}
+		std::ofstream ofs(outFile, std::ios::out | std::ios::trunc); // clear contents
+		ofs.close();
+	}
+	return commands;
+}
+
+void sendMessage(const std::string& message) {
+	fs::path inFile(workingDirectory / "in");
+	std::ofstream ofs(inFile, std::ios::out | std::ios::trunc);
+	ofs << message;
+	ofs.close();
 }
