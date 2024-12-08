5a5bd35cdbc4b589950a2535e0fcec3608088608
@clasher113 <kizinvanya@gmail.com>
2024-12-08 16:19:29 +0200

    add projector animation

--- projector/scripts/components/projector.lua
+++ projector/scripts/components/projector.lua
@@ -1,3 +1,48 @@
+require "projector:display"
+require "projector:synchronizer"
+
+local skeleton = entity.skeleton
+local transform = entity.transform
+
+local projector_bone_index = skeleton:index("projector")
+local disk_1_bone_index = skeleton:index("disk_1")
+local disk_2_bone_index = skeleton:index("disk_2")
+
 function on_attacked(attacker, pid)
 	entity:despawn()
+end
+
+function on_update(tps)
+	local dst_pos = {DISPLAY.position_x + DISPLAY.offset_x, DISPLAY.position_y + DISPLAY.offset_y, DISPLAY.position_z + DISPLAY.offset_z}
+	local size
+
+	if (DISPLAY.orientation == 1) then
+		if (DISPLAY.axis == 1) then
+			size = {DISPLAY.resolution_x, DISPLAY.resolution_y, 1.0}
+		elseif (DISPLAY.axis == 2) then
+			size = {1.0, DISPLAY.resolution_y, DISPLAY.resolution_x}
+		end
+	elseif (DISPLAY.orientation == 2) then
+		if (DISPLAY.axis == 1) then
+			size = {DISPLAY.resolution_x, 1.0, DISPLAY.resolution_y}
+		elseif (DISPLAY.axis == 2) then
+			size = {DISPLAY.resolution_y, 1.0, DISPLAY.resolution_x}
+		end
+	end
+	dst_pos = vec3.add(dst_pos, vec3.div(size, 2))
+
+	local entity_pos = transform:get_pos()
+	local direction = vec3.div(vec3.sub(dst_pos, entity_pos), math.sqrt(vec3.length(vec3.sub(dst_pos, entity_pos))))
+	local rotation_x = math.atan2(-direction[1], -direction[3]) * 180.0 / math.pi
+	local rotation_y = math.atan(direction[2]) * 180.0 / math.pi
+
+	local matrix = mat4.rotate({0, 1, 0}, rotation_x)
+	matrix = mat4.rotate(matrix, {1, 0, 0}, rotation_y)
+
+	skeleton:set_matrix(projector_bone_index, matrix)
+	if (SYNC.is_capturing == true) then
+		matrix = mat4.rotate({1, 0, 0}, time.uptime() % 360 * 50)
+		skeleton:set_matrix(disk_1_bone_index, matrix)
+		skeleton:set_matrix(disk_2_bone_index, matrix)
+	end
 end
--- projector/scripts/projector.lua
+++ projector/scripts/projector.lua
@@ -1,4 +1,5 @@
 require "projector:display"
+require "projector:synchronizer"
 
 function on_interact(x, y, z, pid)
     hud.show_overlay("projector:projector")
@@ -12,12 +13,11 @@ function on_placed(x, y, z, playerid)
     entities.spawn("projector:projector_entity", {math.floor(x) + 0.5, y + 0.5, math.floor(z) + 0.5})
 end
 
-function on_block_break_by(x, y, z, playerid)
-    on_broken(x, y, z, playerid)
-    print("test")
-end
-
 function on_broken(x, y, z, playerid)
+    if (SYNC.is_capturing == true) then
+        block.set(x, y, z, block.index("projector:projector"), 0)
+        return
+    end
     local entities_arr = entities.get_all_in_box({x, y, z}, {1, 1, 1})
     if (entities_arr == nil) then
         return
